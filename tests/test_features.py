cf6d1376117f5bc654a889f16becde5eeb7ce4c3
Vincent ROGER <roger.vins.11@gmail.com>
2020-07-23 22:35:25 +0200

    Add log Mel Spectrogram feature.

--- tests/test_features.py
+++ tests/test_features.py
@@ -5,6 +5,7 @@ import pytest
 from audio_loader.features.raw_audio import WindowedAudio
 from audio_loader.features.mfcc import WindowedMFCC
 from audio_loader.features.log_spectrogram import WindowedLogSpectrogram
+from audio_loader.features.log_mel import WindowedLogMel
 
 
 class TestRawAudio:
@@ -79,3 +80,24 @@ class TestLogSpectrogram:
         assert log_spec_normalized.shape[-2] == 1 + int((len(signal) - 1024 + 1024) / 512)
         assert log_spec_no_overlap.shape[-1] == 1 + 1024/2
         assert log_spec_no_overlap.shape[-2] == 1 + int((len(signal) - 1024 + 1024) / 1024)
+
+class TestLogMel:
+    """Test the log mel module."""
+
+    def test_windowed(self, testing_audio):
+        """Test the WindowedMFCC class."""
+        signal, samplerate = testing_audio
+        try:
+            feature_extractor = WindowedLogMel(1024, 512, 16000, n_mels=32, normalize=True)
+            log_mel_normalized = feature_extractor.process(signal, samplerate)
+            feature_extractor_no = WindowedLogMel(1024, 1024, 16000, n_mels=32, normalize=True)
+            log_mel_no_overlap = feature_extractor_no.process(signal, samplerate)
+
+        except Exception as exception:
+            pytest.fail(f"Unexpected  error: {exception}")
+
+        assert (log_mel_normalized.max() <= 1) and (log_mel_normalized.min() >= -1)
+        assert log_mel_normalized.shape[-1] == 32
+        assert log_mel_normalized.shape[-2] == 1 + int((len(signal) - 1024 + 1024) / 512)
+        assert log_mel_no_overlap.shape[-1] == 32
+        assert log_mel_no_overlap.shape[-2] == 1 + int((len(signal) - 1024 + 1024) / 1024)
