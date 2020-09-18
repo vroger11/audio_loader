c70461dcea90def4b9b5e307b650440845d1ded5
Vincent ROGER <roger.vins.11@gmail.com>
2020-09-18 16:56:04 +0200

    Update test API usage.

--- tests/test_pytorch_frontend.py
+++ tests/test_pytorch_frontend.py
@@ -5,7 +5,7 @@ import pytest
 from audio_loader.features.raw_audio import WindowedAudio
 from audio_loader.features.mfcc import WindowedMFCC
 from audio_loader.ground_truth.timit import TimitGroundTruth
-from audio_loader.dl_frontends.pytorch.fill_ram import get_pytorch_dataloader_fill_ram
+from audio_loader.dl_frontends.pytorch.fill_ram import get_dataloader_fixed_size
 from audio_loader.samplers.windowed_segments import WindowedSegmentSampler
 
 
@@ -18,7 +18,7 @@ class TestsPytorch:
         mfcc_extractor = WindowedMFCC(1024, 512, 16000, n_mfcc=32, normalize=True)
         sampler = WindowedSegmentSampler([audio_extractor, mfcc_extractor], gt_getter, 8000)
         try:
-            get_pytorch_dataloader_fill_ram(sampler, 32, "train")
-            get_pytorch_dataloader_fill_ram(sampler, 16, "test")
+            get_dataloader_fixed_size(sampler, 32, "train")
+            get_dataloader_fixed_size(sampler, 16, "test")
         except Exception as exception:
             pytest.fail(f"Unexpected  error: {exception}")
