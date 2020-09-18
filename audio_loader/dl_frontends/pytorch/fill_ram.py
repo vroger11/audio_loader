e6b568a0330c5e7738b78beedc0631571fcc8562
Vincent ROGER <roger.vins.11@gmail.com>
2020-09-18 15:11:24 +0200

    Add function to fill the ram with dynamic size samples.

--- audio_loader/dl_frontends/pytorch/fill_ram.py
+++ audio_loader/dl_frontends/pytorch/fill_ram.py
@@ -1,21 +1,98 @@
-"""Module to get a sampler that fill the CPU RAM."""
+"""Module to get dataloaders that fill the CPU RAM."""
+import torch
+
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 from torch.utils.data import DataLoader, Dataset
 
 
-def get_pytorch_dataloader_fill_ram(sampler_iterator, batch_size, selected_set):
-    """Create dataloader that fill the CPU RAM."""
+def get_dataloader_fixed_size(sampler, batch_size, selected_set):
+    """Create dataloader that fill the CPU RAM with segments of the same size.
 
-    class DatasetFromArray(Dataset):
-        """Create pytorch Dataset from arrays."""
-        def __init__(self, data):
-            self.data = data
+    Parameters:
+    -----------
+    sampler: Sampler
+        audio_loader sampler that sample segments with the same size.
 
-        def __len__(self):
-            return len(self.data)
+    batch_size: int
+        Number of samples per batch.
 
-        def __getitem__(self, index):
-            return self.data[index]
+    selected_set: str
+       can be "train" or "training" for the training set
+              'valid', 'validation', 'dev' or "devel" for the validation set
+              'test' or "testing" for the test set.
 
+    Return:
+    -------
+    Dataloader designed for fixed segments size.
+    """
     train_set = selected_set in ["train", "training"]
-    dataset = DatasetFromArray(list(sampler_iterator.get_samples_from(selected_set)))
+    dataset = DatasetFromArray(list(sampler.get_samples_from(selected_set)))
     return DataLoader(dataset, batch_size=batch_size, shuffle=train_set, drop_last=train_set)
+
+
+def get_dataloader_dynamic_size(sampler, batch_size, selected_set):
+    """Create dataloader that fill the CPU RAM with segments of different sizes.
+
+    Parameters:
+    -----------
+    sampler: Sampler
+        audio_loader sampler that samples segments with the same size.
+
+    batch_size: int
+        Number of samples per batch.
+
+    selected_set: str
+       can be "train" or "training" for the training set
+              'valid', 'validation', 'dev' or "devel" for the validation set
+              'test' or "testing" for the test set.
+
+    Return:
+    -------
+    Dataloader with padded samples designed for RNN. The ground truth is not
+    padded. Each batch return packed padded sequences.
+    """
+    train_set = selected_set in ["train", "training"]
+    data = [(torch.tensor(x.transpose(1, 0, 2)), y) for x, y in sampler.get_samples_from(selected_set)]
+    dataset = DatasetFromArray(list(data))
+
+    if sampler.supervised:
+        pad_collate_func = pad_collate_supervised
+    else:
+        pad_collate_func = pad_collate_unsupervised
+
+    return DataLoader(dataset, batch_size=batch_size,
+                      shuffle=train_set, drop_last=train_set,
+                      collate_fn=pad_collate_func)
+
+
+def pad_collate_supervised(batch):
+    """Collate function to pad data of supervised batch."""
+    (data, target) = zip(*batch)
+    data_lens = [len(x) for x in data]
+    data_padded = pad_sequence(data, batch_first=True, padding_value=0.)
+    packed_data = pack_padded_sequence(data_padded, data_lens,
+                                       batch_first=True, enforce_sorted=False)
+    return packed_data, target
+
+
+def pad_collate_unsupervised(batch):
+    """Collate function to pad data of unsupervised batch."""
+    data = zip(*batch)
+    data_lens = [len(x) for x in data]
+    data_padded = pad_sequence(data, batch_first=True, padding_value=0.)
+    packed_data = pack_padded_sequence(data_padded, data_lens,
+                                       batch_first=True, enforce_sorted=False)
+    return packed_data
+
+
+class DatasetFromArray(Dataset):
+    """Create pytorch Dataset from a list of arrays."""
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
--- example_pytorch.py
+++ example_pytorch.py
@@ -1,3 +1,4 @@
+"""Library usage to create pytorch dataloaders with timit dataset."""
 from os.path import join
 from pathlib import Path
 
@@ -5,7 +6,7 @@ from audio_loader.features.raw_audio import WindowedAudio
 from audio_loader.features.mfcc import WindowedMFCC
 from audio_loader.ground_truth.timit import TimitGroundTruth
 from audio_loader.samplers.windowed_segments import WindowedSegmentSampler
-from audio_loader.dl_frontends.pytorch.fill_ram import get_pytorch_dataloader_fill_ram
+from audio_loader.dl_frontends.pytorch.fill_ram import get_dataloader_fixed_size
 
 timit_gt = TimitGroundTruth(join(Path.home(), "data/darpa-timit-acousticphonetic-continuous-speech"))
 print("groundtruth loaded")
@@ -13,15 +14,15 @@ print("groundtruth loaded")
 raw_feature_processor = WindowedAudio(1024, 512, 16000)
 raw_sampler = WindowedSegmentSampler([raw_feature_processor], timit_gt, 8000, overlap=0.5)
 
-raw_train_dataloader = get_pytorch_dataloader_fill_ram(raw_sampler, 32, "train")
-raw_test_dataloader = get_pytorch_dataloader_fill_ram(raw_sampler, 32, "test")
+raw_train_dataloader = get_dataloader_fixed_size(raw_sampler, 32, "train")
+raw_test_dataloader = get_dataloader_fixed_size(raw_sampler, 32, "test")
 
 print("raw audio done")
 
 mfcc_feature_processor = WindowedMFCC(1024, 512, 16000, 20)
 mfcc_sampler = WindowedSegmentSampler([mfcc_feature_processor], timit_gt, 8000, overlap=0.5)
-mfcc_train_dataloader = get_pytorch_dataloader_fill_ram(mfcc_sampler, 32, "train")
-mfcc_test_dataloader = get_pytorch_dataloader_fill_ram(mfcc_sampler, 32, "test")
-
+mfcc_train_dataloader = get_dataloader_fixed_size(mfcc_sampler, 32, "train")
+mfcc_test_dataloader = get_dataloader_fixed_size(mfcc_sampler, 32, "test")
 
 print("mfcc done")
+
