f422efd171786cac942083c4294394b3054d8dc4
Vincent ROGER <roger.vins.11@gmail.com>
2020-09-25 16:01:23 +0200

    Add dataset getter alongside getter for dataloader.

--- audio_loader/dl_frontends/pytorch/fill_ram.py
+++ audio_loader/dl_frontends/pytorch/fill_ram.py
@@ -5,6 +5,26 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 from torch.utils.data import DataLoader, Dataset
 
 
+def get_dataset_fixed_size(sampler, selected_set):
+    """Create dataset that fill the CPU RAM with segments of the same size.
+
+    Parameters:
+    -----------
+    sampler: Sampler
+        audio_loader sampler that sample segments with the same size.
+
+    selected_set: str
+       can be "train" or "training" for the training set
+              'valid', 'validation', 'dev' or "devel" for the validation set
+              'test' or "testing" for the test set.
+
+    Return:
+    -------
+    Dataset designed for fixed segments size.
+    """
+    return DatasetFromArray(list(sampler.get_samples_from(selected_set)))
+
+
 def get_dataloader_fixed_size(sampler, batch_size, selected_set):
     """Create dataloader that fill the CPU RAM with segments of the same size.
 
@@ -26,8 +46,39 @@ def get_dataloader_fixed_size(sampler, batch_size, selected_set):
     Dataloader designed for fixed segments size.
     """
     train_set = selected_set in ["train", "training"]
-    dataset = DatasetFromArray(list(sampler.get_samples_from(selected_set)))
-    return DataLoader(dataset, batch_size=batch_size, shuffle=train_set, drop_last=train_set)
+    return DataLoader(get_dataset_fixed_size(sampler, selected_set),
+                      batch_size=batch_size, shuffle=train_set, drop_last=train_set)
+
+
+def get_dataset_dynamic_size(sampler, selected_set):
+    """Create dataset that fill the CPU RAM with segments of different sizes.
+
+    Parameters:
+    -----------
+    sampler: Sampler
+        audio_loader sampler that samples segments with the same size.
+
+    selected_set: str
+       can be "train" or "training" for the training set
+              'valid', 'validation', 'dev' or "devel" for the validation set
+              'test' or "testing" for the test set.
+
+    Return:
+    -------
+    Dataloader with padded samples designed for RNN. The ground truth is not
+    padded. Each batch return packed padded sequences.
+    """
+    # multichannel not supported for pytorch
+    # TODO find a solution for multichannels
+    data = [(torch.tensor(x.reshape(*x.shape[1:])), torch.tensor(y)) for x, y in sampler.get_samples_from(selected_set)]
+    dataset = DatasetFromArray(list(data))
+
+    if sampler.supervised:
+        pad_collate_func = pad_collate_supervised
+    else:
+        pad_collate_func = pad_collate_unsupervised
+
+    return dataset, pad_collate_func
 
 
 def get_dataloader_dynamic_size(sampler, batch_size, selected_set):
@@ -52,17 +103,7 @@ def get_dataloader_dynamic_size(sampler, batch_size, selected_set):
     padded. Each batch return packed padded sequences.
     """
     train_set = selected_set in ["train", "training"]
-
-    # multichannel not supported for pytorch
-    # TODO find a solution for multichannels
-    data = [(torch.tensor(x.reshape(*x.shape[1:])), torch.tensor(y)) for x, y in sampler.get_samples_from(selected_set)]
-    dataset = DatasetFromArray(list(data))
-
-    if sampler.supervised:
-        pad_collate_func = pad_collate_supervised
-    else:
-        pad_collate_func = pad_collate_unsupervised
-
+    dataset, pad_collate_func = get_dataset_dynamic_size(sampler, selected_set)
     return DataLoader(dataset, batch_size=batch_size,
                       shuffle=train_set, drop_last=train_set,
                       collate_fn=pad_collate_func)
